a0d465f830f679ed52f1e68a6cd4008bec70c957
David <david.tello2012@gmail.com>
2017-09-26 13:37:08 -0700

    code clean up
    fixes lint errors, added more comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,42 +1,63 @@
 import fresh_tomatoes
 import media
 
-# Usung the Movie class from the media file we create a list of movies to be displayed
-batman_v_superman = media.Movie('Batman v Superman',
-                                'https://upload.wikimedia.org/wikipedia/en/2/20/Batman_v_Superman_poster.jpg',
-                                'https://www.youtube.com/watch?v=0WWzgGyAH6Y')
+# basepath cleans up the code and lint errors
+base_path = 'https://upload.wikimedia.org/wikipedia'
 
-logan = media.Movie('Logan',
-                    'https://upload.wikimedia.org/wikipedia/en/3/37/Logan_2017_poster.jpg',
-                    'https://www.youtube.com/watch?v=XaE_9pfybL4')
-
-avengers = media.Movie('Avengers',
-                       'https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg',
-                       'https://www.youtube.com/watch?v=eOrNdBpGMv8')
-
-suicide_squad = media.Movie('Suicide Squad',
-                            'https://upload.wikimedia.org/wikipedia/en/5/50/Suicide_Squad_%28film%29_Poster.png',
-                            'https://www.youtube.com/watch?v=mMb-RrhTxIE')
-
-jurassic_park = media.Movie('Jurassic Park',
-                            'https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg',
-                            'https://www.youtube.com/watch?v=lc0UehYemQA')
-
-
-jaws = media.Movie('Jaws',
-                   'https://upload.wikimedia.org/wikipedia/en/e/eb/JAWS_Movie_poster.jpg',
-                   'https://www.youtube.com/watch?v=U1fu_sA7XhE')
-
-back_to_the_future = media.Movie('Back To The Future',
-                                 'https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg',
-                                 'https://www.youtube.com/watch?v=qvsgGtivCgs')
-
-the_neverEnding_story = media.Movie('The NeverEnding Story',
-                                    'https://upload.wikimedia.org/wikipedia/en/9/9b/Neverendingstoryposter.jpg',
-                                    'https://www.youtube.com/watch?v=UeFni9dOv7c')
+# Usung the Movie class from the media file we
+# create a list of movies to be displayed
+batman_v_superman = media.Movie(
+  'Batman v Superman',
+  base_path + '/en/2/20/Batman_v_Superman_poster.jpg',
+  'https://www.youtube.com/watch?v=0WWzgGyAH6Y'
+  )
+logan = media.Movie(
+  'Logan',
+  base_path + '/en/3/37/Logan_2017_poster.jpg',
+  'https://www.youtube.com/watch?v=XaE_9pfybL4'
+  )
+avengers = media.Movie(
+  'Avengers',
+  base_path + '/en/f/f9/TheAvengers2012Poster.jpg',
+  'https://www.youtube.com/watch?v=eOrNdBpGMv8'
+  )
+suicide_squad = media.Movie(
+  'Suicide Squad',
+  base_path + '/en/5/50/Suicide_Squad_%28film%29_Poster.png',
+  'https://www.youtube.com/watch?v=mMb-RrhTxIE'
+  )
+jurassic_park = media.Movie(
+  'Jurassic Park',
+  base_path + '/en/e/e7/Jurassic_Park_poster.jpg',
+  'https://www.youtube.com/watch?v=lc0UehYemQA'
+  )
+jaws = media.Movie(
+  'Jaws',
+  base_path + '/en/e/eb/JAWS_Movie_poster.jpg',
+  'https://www.youtube.com/watch?v=U1fu_sA7XhE'
+  )
+back_to_the_future = media.Movie(
+  'Back To The Future',
+  base_path + '/en/d/d2/Back_to_the_Future.jpg',
+  'https://www.youtube.com/watch?v=qvsgGtivCgs'
+  )
+the_neverEnding_story = media.Movie(
+  'The NeverEnding Story',
+  base_path + '/en/9/9b/Neverendingstoryposter.jpg',
+  'https://www.youtube.com/watch?v=UeFni9dOv7c'
+  )
 
 # Movies array containing the instaces of the Movie Class
-movies = [ batman_v_superman, logan, avengers, suicide_squad, jurassic_park, jaws, back_to_the_future, the_neverEnding_story]
+movies = [
+  batman_v_superman,
+  logan, avengers,
+  suicide_squad,
+  jurassic_park,
+  jaws,
+  back_to_the_future,
+  the_neverEnding_story
+]
 
-# movie website gets created using the fresh_tomatoes class by passing the array of movies
+# movie website gets created using the fresh_tomatoes
+# class by passing the array of movies
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,16 +1,14 @@
 import webbrowser
 
+
 class Movie():
     """
-    This class provides a way to create movie related information for the final project
-    The following have to be provided
+    This class provides a way to create movie related information
+    for the final project. The following have to be provided
     - Name of the movie
     - Box art / poster url
     - youtube url
-
     """
-    
-    
     def __init__(self, movie_title, poster_image, trailer_youtube):
         # Sets instance variables to the values provided
         self.title = movie_title
